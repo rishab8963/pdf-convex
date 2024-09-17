b1263ef954fc760d1ca3c5ed046382111c8705ae
rishab8963 <rishabhraj13122003@gmail.com>
2024-09-17 11:34:22 +0530

    model_into_functions

--- flask-backend/MLmodel/project_convex/model.py
+++ flask-backend/MLmodel/project_convex/model.py
@@ -31,75 +31,85 @@ text_splitter = RecursiveCharacterTextSplitter(
 model2 = Llama(model_path="./bge-small-en-v1.5-f16.gguf", embedding=True, verbose=False)
 
 
-text = ""
-# Open a document
-for doc in documents:
-    doc_ = pymupdf.open(`./documents/${doc}.pdf`)
+# def load():
+#     text = ""
+#     # Open a document
+#     for doc in documents:
+#         doc_ = pymupdf.open(`./documents/${doc}.pdf`)
 
-# Extract all the text from the pdf document
-    for page in doc_:
-        result = page.get_text()
-        text += result
+#     # Extract all the text from the pdf document
+#         for page in doc_:
+#             result = page.get_text()
+#             text+= result
+#     return text        
 
 
-documents = text_splitter.create_documents([text])
+def storeVectorDB(text):
+    # delete class "Article" - THIS WILL DELETE ALL DATA IN THIS CLASS
+    client.schema.delete_class("Pdf")
 
-document_embeddings = []
-# Generate Embeddings for every single document in documents and append it into document_embeddings
-for document in documents:
-    embeddings = model2.create_embedding([document.page_content])
-    document_embeddings.append({
-        "content": document.page_content,
-        "embedding": embeddings["data"][0]["embedding"]
+    # Weaviate schema creation (if not already created)
+    class_obj = {
+        "class": "Pdf",
+        "vectorizer": "none",
+    }
+
+    # Create the schema in Weaviate
+    client.schema.create_class(class_obj)
+    documents = text_splitter.create_documents([text])
+
+    document_embeddings = []
+    # Generate Embeddings for every single document in documents and append it into document_embeddings
+    for document in documents:
+        embeddings = model2.create_embedding([document.page_content])
+        document_embeddings.append({
+            "content": document.page_content,
+            "embedding": embeddings["data"][0]["embedding"]
     })
-
-# Insert embeddings into Weaviate
-client.batch.configure(batch_size=100)
-with client.batch as batch:
-    for doc in document_embeddings:
-        properties = {
-            "content": doc["content"],
-        }
+    # Insert embeddings into Weaviate
+    client.batch.configure(batch_size=100)
+    with client.batch as batch:
+        for doc in document_embeddings:
+            properties = {
+              "content": doc["content"],
+            }
         
-        batch.add_data_object(properties, "Pdf", vector=doc['embedding'])
-    
+            batch.add_data_object(properties, "Pdf", vector=doc['embedding'])
+     
 
-query ="What is the main idea of this"
-# Generate Embeddings for every single document in documents and append it into document_embeddings
-query_embeddings = model2.create_embedding(query)
-query_vector=query_embeddings['data'][0]["embedding"]
-print((query_vector))
+def sawal(query_user):
+    # Generate Embeddings for every single document in documents and append it into document_embeddings
+    query_embeddings = model2.create_embedding(query_user)
+    query_vector=query_embeddings['data'][0]["embedding"]
 
-nearVector = {
-    "vector": query_vector
-}
+    nearVector = {
+        "vector": query_vector
+    }
 
-result = (
-    client.query.get("Pdf", ["content"])
-    .with_near_vector(
-        nearVector
+    result = (
+        client.query.get("Pdf", ["content"])
+        .with_near_vector(
+            nearVector
+        )
+        .with_limit(5)
+        .with_additional(['certainty'])
+        .do()
     )
-    .with_limit(5)
-    .with_additional(['certainty'])
-    .do()
-)
-# Print the result
-result = result['data']['Get']['Pdf']
-print("Query result:", result)
+    # Print the result
+    result = result['data']['Get']['Pdf']
+    # print("Query result:", result)
 
-
-
-
-stream=model.create_chat_completion(
-  messages=[
-      {"role": "user", "content": template.format(
-          context="\n\n".join(result[0]['content']),
-          question=query
-      )}
-  ]
-# ,
-#   stream=True
-)
+    response=model.create_chat_completion(
+    messages=[
+        {"role": "user", "content": template.format(
+            context="\n\n".join(result[0]['content']),
+            question=query_user
+        )}
+    ]
+    # ,
+    #   stream=True
+    )
+    return response['choices'][0]['message']['content']
 
 # for chunk in stream:
 #     print(chunk['choices'][0]['delta'].get('content', ''), end='')
